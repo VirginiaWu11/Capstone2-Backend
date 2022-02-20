c82818c260e115ab753bf832dce0095cb6a0c128
Virginia Wu <virginiawu11@gmail.com>
2022-02-20 12:18:21 -0500

    add tests

--- test_user_model.py
+++ test_user_model.py
@@ -20,14 +20,14 @@ db.create_all()
 
 
 class UserModelTestCase(TestCase):
-    """Test views for messages."""
+    """Test views for users."""
 
     def setUp(self):
         """Create test client, add sample data."""
         db.drop_all()
         db.create_all()
 
-        u1 = User(
+        user1 = User(
             username="testuser1",
             first_name="test1",
             last_name="user1",
@@ -35,10 +35,18 @@ class UserModelTestCase(TestCase):
             password="HASHED_PASSWORD",
         )
 
-        db.session.add(u1)
+        db.session.add(user1)
         db.session.commit()
 
         self.client = app.test_client()
+
+        user3 = User.signup(
+            username="testuser3",
+            first_name="test3",
+            last_name="user3",
+            email="testuser3@email.com",
+            password="HASHED_PASSWORD3",
+        )
 
     def tearDown(self):
         db.session.rollback()
@@ -46,7 +54,7 @@ class UserModelTestCase(TestCase):
     def test_user_model(self):
         """Does basic model work?"""
 
-        u2 = User(
+        user2 = User(
             username="testuser2",
             first_name="test2",
             last_name="user2",
@@ -54,18 +62,18 @@ class UserModelTestCase(TestCase):
             password="HASHED_PASSWORD2",
         )
 
-        db.session.add(u2)
+        db.session.add(user2)
         db.session.commit()
 
         # User should have no pins record & no assets
-        self.assertEqual(len(u2.pins), 0)
-        self.assertEqual(len(u2.assets), 0)
+        self.assertEqual(len(user2.pins), 0)
+        self.assertEqual(len(user2.assets), 0)
 
     # SignUp Tests
     def test_valid_user_signup(self):
         """Test user method signup"""
 
-        u2 = User.signup(
+        user2 = User.signup(
             username="testuser2",
             first_name="test2",
             last_name="user2",
@@ -73,5 +81,28 @@ class UserModelTestCase(TestCase):
             password="HASHED_PASSWORD2",
         )
 
-        self.assertNotEqual(u2.password, "HASHED_PASSWORD2")
-        self.assertEqual(len(User.query.all()), 2)
+        self.assertNotEqual(user2.password, "HASHED_PASSWORD2")
+        self.assertEqual(len(User.query.all()), 3)
+
+    def test_invalid_username_signup(self):
+
+        with self.assertRaises(exc.IntegrityError) as context:
+            invalid_user = User.signup(
+                username="testuser1",
+                first_name="test1",
+                last_name="user1",
+                email="testuser1@email.com",
+                password="HASHED_PASSWORD",
+            )
+
+    # Authentication tests
+
+    def test_valid_authentication(self):
+        user = User.authenticate("testuser3", "HASHED_PASSWORD3")
+        self.assertIsNotNone(user)
+
+    def test_invalid_username(self):
+        self.assertFalse(User.authenticate("testuser2", "HASHED_PASSWORD"))
+
+    def test_wrong_password(self):
+        self.assertFalse(User.authenticate("testuser3", "incorrect"))
